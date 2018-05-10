d60f4067ce1fd3efc2ab25a3982ccfa3f418b49d
John Krull <john.a.krull@gmail.com>
2018-05-10 18:11:06 -0500

    Fresh, simpler version of the bot

---
+++ bot.js
@@ -0,0 +1,91 @@
+const { WebClient } = require('@slack/client');
+const { format } = require('util');
+const { bot, minutesBetweenExercises, messages, exercises } = require('./config');
+
+// An access token (from your Slack app or custom integration - xoxp, xoxb, or xoxa)
+const token = process.env.SLACK_TOKEN;
+const web = new WebClient(token);
+
+// Will be set via getConversationId
+let conversationId = 'tbd';
+
+/**
+ * Gets a random integer up to the max number provided.
+ * 
+ * @param {Number} max
+ *     The maximum integer to get
+ */
+function getRandomInt (max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+/**
+ * Accepts a template and variables to populate the template with, and sends
+ * that message to the configured channel.
+ * 
+ * @param {*} params
+ *     Any number of parameters to create a message. The first argument should
+ *     be the string to be formatted, followed by any arguments that the string
+ *     might need.
+ */
+function sendMessage (...params) {
+    // See: https://api.slack.com/methods/chat.postMessage
+    web.chat.postMessage({
+        channel: conversationId,
+        text: format.apply(null, params),
+        as_user: false,
+        username: bot.username,
+        icon_emoji: bot.icon_emoji
+    })
+    .then((res) => {
+        // `res` contains information about the posted message
+        console.log('Sent message (', res.message.text, ') at', res.ts);
+    })
+    .catch(console.error);
+}
+
+/**
+ * Sends a random stretch message to all users in the channel.
+ */
+function sendRandomStretchMessage () {
+    let exerciseIndex = getRandomInt(exercises.stretches.length - 1);
+    let exercise = exercises.stretches[exerciseIndex];
+
+    sendMessage('Time to stretch, <!here|here>!', '\n*' + exercise[0] + '*\n_' + exercise[1] + '_');
+    sendMessage(messages.announcement_upcoming, minutesBetweenExercises.stretches);
+}
+
+/**
+ * Searches the list of conversations for one matching the given name, then
+ * passes the ID of that conversation to the callback function. If the channel
+ * cannot be found, the callback will not be called.
+ * 
+ * @param {String} name
+ *     The name of the channel being looked for.
+ * @param {Function} callback
+ *     The function to give the conversation id to.
+ */
+function getConversationId (name, callback) {
+    web.conversations.list()
+    .then(res => {
+        let channels = res.channels;
+
+        channels = channels.filter(channel => {
+            return (channel.name_normalized === name);
+        });
+
+        if (!channels[0].id) {
+            return console.error(`Unable to find channel named "${name}"`);
+        }
+
+        callback(channels[0].id);
+    })
+    .catch(console.error);
+}
+
+getConversationId(process.env.SLACK_CHANNEL, id => {
+    console.log(`Will post messages to channel "${process.env.SLACK_CHANNEL}" (${id})`);
+    conversationId = id;
+    sendMessage(messages.greeting, minutesBetweenExercises.stretches);
+    setInterval(sendRandomStretchMessage, minutesBetweenExercises.stretches * 60000);
+});
---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    bot: {
+        username: 'Arnold',
+        icon_emoji: ':arnold:'
+    },
+    minutesBetweenExercises: {
+        stretches: 1
+    },
+    messages: {
+        "greeting": "GET READY TO STRETCH IN %d MINUTES, <!here|here>",
+        "announcement_upcoming": "NEXT ANNOUNCEMENT IN %d MINUTES"
+    },
+    exercises: {
+        stretches: [
+            // From https://draxe.com/exercise-to-do-at-your-desk/
+            ['Rubber Neck', `Sit up tall and drop your right  ear down towards your right shoulder (you don’t have to touch it!) and hold for a few seconds and repeat for the left side.`],
+            ['Reach for the Stars', `Interlace your fingers and reach up towards the sky, as high as you can … keeping your palms facing up towards the ceiling.`],
+            ['Look Around', `Turn your head the left and try and look over your shoulder and hold for a few seconds … repeat on the right.`],
+            ['Bobblehead', `Drop your chin down towards your chest and GENTLY roll your head from side to side.`],
+            ['Shrugs', `Raise both shoulders up towards your ears and hold for a few seconds and release. Repeat a few times for good measure.`],
+            ['Chest Opener', `Bring your hands behind your back, press your palms together, sit up tall and hold for 5–10 seconds.`],
+            ['Seated Toy Soldier', `Sit up tall and extend your right arm all the way up towards the ceiling. Straighten your left leg out and raise it up as you bring your right arm down and try to touch your left foot. Do 8–10 on each side.`],
+            ['Knee Hugger', `With a bent knee, lift your right leg up and grab it with your arms and pull it in as close to your chest as you can. Hold for 5–10 seconds and make sure and do it on the left side, too.`],
+            ['Reach and Bend', `Extend your right arm over your head and reach out as far as you can to the left and gently bend over. Hold for a few seconds and do it the other way.`],
+            ['Knee Press', `This one stretches out the glutes. With your right ankle on your left knee, gently press against the right knee a few times. Of course, after you’re done with the right side, be sure and give the left side some love, too.`],
+        ]
+    }
+};
